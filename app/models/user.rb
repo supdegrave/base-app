79577ddb05d253406656fcc3f943b20883e1d4cc
Stuart Updegrave <stuart@updegrave.com>
2013-12-08 08:58:44 +0100

    rails_apps_composer: set up database

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :invitable, :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 end
