4747b8c7801098a3c7736826120d83137f0f276c
Stuart Updegrave <stuart@updegrave.com>
2013-12-08 14:16:31 +0100

    disable generation of controller specs by default

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,9 @@ module BaseApp
       g.test_framework :rspec, fixture: true
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
       
-      
       g.view_specs false
       g.helper_specs false
+      g.controller_specs false 
     end
 
     # Settings in config/environments/* take precedence over those specified here.
